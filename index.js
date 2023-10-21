3250e5023ccb366bae7a374ab8cfb0e071719264
jisanahamed09205 <jisanahamed09205@gmail.com>
2023-10-21 23:16:28 +0600

    alhamdulillah server done

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/productDetails/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query)
+      res.send(result);
+    })
+
     app.get('/productsKaku/:id',async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
